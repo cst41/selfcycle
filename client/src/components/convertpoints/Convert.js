2090045214d6cea4c57eb1bb0d93fdb6f63d3dca
cst41 <chunfreak96@gmail.com>
2020-06-28 10:24:42 +0800

    small changes

--- client/src/components/convertpoints/Convert.js
+++ client/src/components/convertpoints/Convert.js
@@ -2,21 +2,31 @@ import React, { Fragment, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types'
 
-const Convert = ({ convertpoints  }) => {
+const Convert = ({ convertpoints, calculatepoints  }) => {
     const [formData, setFormData] = useState({
 
         points: '',
-        emoney: '',
+        eWallet: '',
         
     });
     
-    const { points, emoney } = formData;
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { points, eWallet } = formData;
+    const onChange = e => setFormData({ ...formData, [e.target.id]: e.target.value });
+
+    const onKeyPress = (e) =>
+        calculatepoints = (point, output) => {
+            var point = e.getElementById("points").value;
+            var output =  parseFloat(point) / 10;
+            e.getElementById("eWallet").value = output;
+        };
+    
     
     const onSubmit = async e =>{ 
         e.preventDefault();
-        convertpoints(points, emoney);
+        convertpoints(points, eWallet);
     }
+
+    
     return (
         <Fragment>
             
@@ -24,23 +34,25 @@ const Convert = ({ convertpoints  }) => {
                 <i className="fas fa-exchange-alt text-primary"></i> Enter the amount of points to be convert into eWallet credit
             </p>
             
-            <form className="form" onSubmit={e => onSubmit(e)}>
+            <form className="form" onSubmit={e => onSubmit(e)} onKeyDown={e => onKeyPress(e)}>
             <img  className='form-img' src={ require('../../img/convert1.png')} /> 
                 <div className="form-small">
                     <input 
                         type="points" 
                         placeholder="Your Points"
-                        name="points" 
+                        id="points" 
                         value={points} 
                         onChange={e => onChange(e)}
-                        required />
+                        onKeyDown={e => onKeyPress(e)}
+                        maxLength={6}
+                        />
                 </div>
                 <div className="form-small">
                 <input
-                    type="emoney"
-                    placeholder="eMoney"
-                    name="emoney"
-                    value={emoney} 
+                    type="eWallet"
+                    //placeholder="eWallet"
+                    id="eWallet"
+                    value={eWallet}
                     onChange={e => onChange(e)}
                 />
                 </div>
@@ -48,11 +60,13 @@ const Convert = ({ convertpoints  }) => {
                 <Link className="btn btn-light my-1"to="/dashboard">Go Back</Link>
         </form>
         </Fragment>
+
     )
 }
 
 Convert.propTypes = {
-
+    convertpoints: PropTypes.number,
+    calculatepoints: PropTypes.func,
 }
 
 export default Convert
