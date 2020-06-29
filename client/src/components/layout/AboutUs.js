7042209b3efe79543a066114db6b73d93c77cdf6
cst41 <chunfreak96@gmail.com>
2020-06-29 16:09:13 +0800

    added aboutus

---
+++ client/src/components/layout/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AboutUs = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+AboutUs.propTypes = {
+
+}
+
+export default AboutUs
